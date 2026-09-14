53ca4087d89ee3e44ce6d91b877a40079cc5b460
tloza94-art <tloza94@gmail.com>
2026-09-13 19:09:51 -0500

    Cache manager ranking assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C="fantasy-cc-v8";
-const SHELL=["./","./index.html","./styles.css","./app.js","./game-status-fix.js","./manifest.webmanifest","./icons/icon.svg"];
+const C="fantasy-cc-v9";
+const SHELL=["./","./index.html","./styles.css","./app.js","./game-status-fix.js","./manager-ranking.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
   self.skipWaiting();
