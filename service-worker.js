afa15c359287f343d5f0c19adbebdd88e3dcc518
tloza94-art <tloza94@gmail.com>
2026-09-14 18:29:51 -0500

    Bump cache for lightweight resume refresh

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C="fantasy-cc-v26";
+const C="fantasy-cc-v27";
 const SHELL=["./","./index.html","./styles.css","./app.js","./player-cache.js","./game-status-fix.js","./manager-ranking.js","./upgrade-popup.js","./resume-refresh.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
