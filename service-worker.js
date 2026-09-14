00f36bc18359b365654c57a5e8e671b4f68aa1d3
tloza94-art <tloza94@gmail.com>
2026-09-14 18:49:33 -0500

    Cache pull-to-refresh assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C="fantasy-cc-v28";
-const SHELL=["./","./index.html","./styles.css","./app.js","./player-cache.js","./game-status-fix.js","./manager-ranking.js","./upgrade-popup.js","./resume-refresh.js","./manifest.webmanifest","./icons/icon.svg"];
+const C="fantasy-cc-v29";
+const SHELL=["./","./index.html","./styles.css","./app.js","./player-cache.js","./game-status-fix.js","./manager-ranking.js","./upgrade-popup.js","./resume-refresh.js","./pull-refresh.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
   self.skipWaiting();
