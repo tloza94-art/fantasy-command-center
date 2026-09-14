9af59fda0b7da95f2e1ef0b00efa2888092f2664
tloza94-art <tloza94@gmail.com>
2026-09-13 19:16:27 -0500

    Refresh cache for Sleeper lineup links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C="fantasy-cc-v9";
+const C="fantasy-cc-v10";
 const SHELL=["./","./index.html","./styles.css","./app.js","./game-status-fix.js","./manager-ranking.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
