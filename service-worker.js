0e79ba896a58f7c4d610b32563cfe77f28678fa8
tloza94-art <tloza94@gmail.com>
2026-09-13 20:50:56 -0500

    Cache upgrade popup UI

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C="fantasy-cc-v16";
-const SHELL=["./","./index.html","./styles.css","./app.js","./game-status-fix.js","./manager-ranking.js","./sleeper-links.js","./manifest.webmanifest","./icons/icon.svg"];
+const C="fantasy-cc-v17";
+const SHELL=["./","./index.html","./styles.css","./app.js","./game-status-fix.js","./manager-ranking.js","./upgrade-popup.js","./sleeper-links.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
   self.skipWaiting();
