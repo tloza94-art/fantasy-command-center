25e0c6f67a8e6c81a2e4c20e0ae23f4ae42e3de4
tloza94-art <tloza94@gmail.com>
2026-09-18 21:43:50 -0500

    Bump cache for pull refresh interaction fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C="fantasy-cc-v29";
+const C="fantasy-cc-v30";
 const SHELL=["./","./index.html","./styles.css","./app.js","./player-cache.js","./game-status-fix.js","./manager-ranking.js","./upgrade-popup.js","./resume-refresh.js","./pull-refresh.js","./manifest.webmanifest","./icons/icon.svg"];
 
 self.addEventListener("install",e=>{
